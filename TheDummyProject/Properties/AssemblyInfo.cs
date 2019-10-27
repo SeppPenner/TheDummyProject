1f6ef3ae8be8abaf60eed4420e87e1ab63b52a6a
SeppPenner <HansM2013@gmx.de>
2019-10-27 22:47:10 +0100

    Updated nuget packages, added GitVersionTask.

--- TheDummyProject/Properties/AssemblyInfo.cs
+++ TheDummyProject/Properties/AssemblyInfo.cs
@@ -23,17 +23,3 @@ using System.Runtime.InteropServices;
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 
 [assembly: Guid("a79a51cd-8a5e-4e02-852c-739e8361ad32")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
